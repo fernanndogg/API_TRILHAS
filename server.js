f0ba0d03600839d75dbdb3daa4e7a054d8de585b
Thaina <thaina.siliro@acad.ifma.edu.br>
2024-06-16 14:40:48 -0300

    refatoracao de registro de usuario e implementacao de login

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 // Rota de cadastro
-app.post('/register', async (req, res) => {
-    const { email, password } = req.body;
+app.post('/user/register', async (req, res) => {
+    const { name, username, age, email, gender, password, knowledgeLevel, reasonsWhy } = req.body;
 
     if (!email || !password) {
         return res.status(400).json({ error: 'Email and password are required' });
@@ -22,13 +22,19 @@ app.post('/register', async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     try {
-        const user = await prisma.user.create({
+        const users = await prisma.users.create({
             data: {
+                name,
+                username,
+                age,
                 email,
+                gender,
+                knowledgeLevel,
+                reasonsWhy,
                 password: hashedPassword,
             },
         });
-        res.status(201).json({ message: 'User created', user });
+        res.status(201).json({ message: 'User created' }); /**OBSERVACAO: MELHORAR OS TRATAMENTOS DE ERROS */
     } catch (error) {
         if (error.code === 'P2002') {
             res.status(400).json({ error: 'Email already exists' });
@@ -37,6 +43,40 @@ app.post('/register', async (req, res) => {
         }
     }
 });
+
+// Rota Cadastro
+
+app.post('/user/login', async (req, res) => {
+
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ error: 'Email and password are required' });
+    }
+
+    try {
+        const user = await prisma.users.findUnique({
+            where: {
+                email: email,
+            }
+        })
+        if (user == null) {
+            return res.json({ message: "Usuario nao existe" })
+        }
+
+        const verifyPassword = bcrypt.compareSync(password, user.password);
+        if (verifyPassword) {
+            res.json(user)
+        } else {
+            res.json({ message: "Senha Incorreta" })
+        }
+
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).json({ error: 'Internal server error' });
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
